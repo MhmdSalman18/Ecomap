429456bbf270a490b05b982951289cf47f796a11
Muhammed Salman <muhammedsalman181818@gmail.com>
2024-12-07 10:21:53 +0530

    Initial commit

--- lib/map.dart
+++ lib/map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HeatMap extends StatefulWidget {
   const HeatMap({super.key});
@@ -8,19 +9,37 @@ class HeatMap extends StatefulWidget {
 }
 
 class _HeatMapState extends State<HeatMap> {
+  late GoogleMapController _mapController;
+  final LatLng _initialPosition = const LatLng(37.7749, -122.4194); // Example: San Francisco
+  final Set<Marker> _markers = {};
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
+        title: const Text("Heat Map"),
       ),
-      drawer: Drawer(),
-      body: Center(
-        child: Column(
-          children: [Text("heat map page")],
+      drawer: const Drawer(),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: _initialPosition,
+          zoom: 10.0,
         ),
+        markers: _markers,
+        onMapCreated: (GoogleMapController controller) {
+          _mapController = controller;
+          setState(() {
+            _markers.add(
+              Marker(
+                markerId: const MarkerId('initial_marker'),
+                position: _initialPosition,
+                infoWindow: const InfoWindow(title: "San Francisco"),
+              ),
+            );
+          });
+        },
       ),
-      
     );
   }
 }
