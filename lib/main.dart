fba5a376174d6692c35f4396ffe0cf198ea33690
Muhammed Salman <muhammedsalman181818@gmail.com>
2024-12-18 09:02:57 +0530

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:ecomap/BottomNavigationBar.dart';
 import 'package:ecomap/REGISTRATION/login.dart';
+import 'package:ecomap/home.dart';
 import 'package:ecomap/uploadstate.dart';
 import 'package:flutter/material.dart';
 
@@ -12,7 +13,7 @@ class EcomapApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: UploadState(title: '',), // Start with BottomNavigationBarExample
+      home: HomePage(title: '',), // Start with BottomNavigationBarExample
     );
   }
 }
