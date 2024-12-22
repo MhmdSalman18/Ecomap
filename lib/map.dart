adb6c970adaaefc97602f262aede2c859cdace0c
Muhammed Salman <muhammedsalman181818@gmail.com>
2024-12-22 09:47:18 +0530

    added map

--- lib/map.dart
+++ lib/map.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:maplibre_gl/maplibre_gl.dart';
 
 class HeatMap extends StatefulWidget {
   const HeatMap({super.key});
@@ -9,10 +8,8 @@ class HeatMap extends StatefulWidget {
 }
 
 class _HeatMapState extends State<HeatMap> {
-  late MaplibreMapController _mapController;
 
   // Initial position of the map
-  final LatLng _initialPosition = const LatLng(37.7749, -122.4194); // Example: San Francisco
 
   @override
   Widget build(BuildContext context) {
@@ -20,27 +17,8 @@ class _HeatMapState extends State<HeatMap> {
       appBar: AppBar(
         title: const Text("Heat Map"),
       ),
-      body: MaplibreMap(
-        styleString: "https://demotiles.maplibre.org/style.json", // Default MapLibre style
-        initialCameraPosition: CameraPosition(
-          target: _initialPosition,
-          zoom: 10.0, // Initial zoom level
-        ),
-        onMapCreated: _onMapCreated,
-        myLocationEnabled: true,
-        myLocationTrackingMode: MyLocationTrackingMode.tracking,
-      ),
+      
     );
   }
 
-  void _onMapCreated(MaplibreMapController controller) {
-    _mapController = controller;
-    // Additional setup if needed, such as adding markers or layers.
-  }
-
-  @override
-  void dispose() {
-    _mapController.dispose();
-    super.dispose();
-  }
 }
