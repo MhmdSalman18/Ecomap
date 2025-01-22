a2049668b87b697f00499c825a7c269f9fbbc3ec
Muhammed Salman <muhammedsalman181818@gmail.com>
2025-01-22 12:30:56 +0530

    logo

--- lib/splash.dart
+++ lib/splash.dart
@@ -8,11 +8,29 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
   @override
   void initState() {
     super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    );
+    _controller.forward();
     _navigateToHome();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   void _navigateToHome() async {
@@ -30,14 +48,20 @@ class _SplashScreenState extends State<SplashScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(
-              width: 200, // Set the desired width
-              height: 200, // Set the desired height
-              child: Image.asset('assets/assets/ecomap_logo.png'),
+            FadeTransition(
+              opacity: _animation,
+              child: SizedBox(
+                width: 200, // Set the desired width
+                height: 200, // Set the desired height
+                child: Image.asset('assets/assets/ecomap_logo.png'),
+              ),
             ),
             const SizedBox(height: 10),
-            const CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            ScaleTransition(
+              scale: _animation,
+              child: const CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              ),
             ),
           ],
         ),
