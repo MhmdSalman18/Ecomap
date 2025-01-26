befd30d8561313451f9dc4a380bdda9317f4b093
Muhammed Salman <muhammedsalman181818@gmail.com>
2025-01-26 19:38:28 +0530

    loading animation

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class EcomapApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(), // St with BottomNavigationBarExample
+      home: SplashScreen(), // St with 
     );
   }
 }
